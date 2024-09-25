bfd4649339264022a2aa64ef758cf7ba37c0dee1
Kevin Bett <prodigy4614@gmail.com>
2024-09-25 07:05:20 +0300

    added functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(act1=`roller-skate`){
+   return(`This Saturday, I want to ${act1}!`);
+}
+
+
+const mondayWork = function (act2="go to the office"){
+    return(`This Monday, I will ${act2}.`);
+}
+
+
+function wrapAdjective(param1 = '*') {
+    return function(param2=`special`) {
+      return `You are ${param1}${param2}${param1}!`;
+    };
+  }
